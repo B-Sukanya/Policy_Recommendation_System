b2927f350ced1edef61dc29db2e75301424f66a0
Sukanya <sukanya012025@outlook.com>
2026-09-17 11:29:34 +0530

    Update app.py

--- Code/app.py
+++ Code/app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="InsurAI | Policy Recommendation",
+    page_title="Insurance | Policy Recommendation",
     page_icon="🛡️",
     layout="wide",
     initial_sidebar_state="expanded",
